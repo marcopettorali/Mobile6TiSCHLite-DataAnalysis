ed3786f99e918cb4f28ae7d0b370055a76960561
Marco Pettorali <marcopettorali1@gmail.com>
2022-05-18 15:33:58 +0200

    scenario updated

--- scenario.py
+++ scenario.py
@@ -33,11 +33,6 @@ def parse_scenario(scenario, reps = 35):
     with open(f"./analysis/{scenario.split('/')[-1]}.json", "a") as file:
         file.write(json.dumps(scenario_ci, indent=4))
         
-<<<<<<< HEAD
 if __name__=="__main__":
     if len(sys.argv) > 2:
         parse_scenario(sys.argv[1], int(sys.argv[2]))
-=======
-if __name__ == "__main__":
-    parse_scenario(sys.argv[1], int(sys.argv[2]))
->>>>>>> 290742baabb1430dabc8292b1fe5650c612c7518
