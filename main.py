3039944d7ca2141f4d05cfbf7c6f759bca86a044
Marco Pettorali <marcopettorali1@gmail.com>
2022-05-31 07:42:17 +0000

    Partial results (up to 70 mns for dddu)

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from itertools import product
 name = f"/home/ubuntu/git/Mobile6TiSCH-Lite/Mobile6TiSCH-Lite/simulations/mobile6tischlite/results"
 
 for s, p, mn in product(["convergecast_sddu4", "reqres_dddu"], [25,50,75], list(range(10,110,10))) :
-    parse_scenario(name + f"/mobility_{s}_p{p}_wc-{mn}")
+    parse_scenario(name + f"/maximumrate_{s}_p{p}-{mn}")
 exit()
 for s, p, mn, sp in product(["convergecast_sddu4", "reqres_dddu"], [25,50,75], list(range(10,110,10)), [0.5,2,5]) :
     parse_scenario(name + f"/speed_{s}_p{p}_wc-{mn},{sp}mps")
